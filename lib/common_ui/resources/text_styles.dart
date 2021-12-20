b406d0b5af1139f6129dafd2794451b8a1c337bb
memishood <emre.memis.49@gmail.com>
2021-12-20 23:31:11 +0300

    add static text styles to app

---
+++ lib/common_ui/resources/text_styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+/// @author emremms35@gmail.com
+class TextStyles {
+  static const regularTextStyle = TextStyle(
+    fontFamily: "Regular"
+  );
+
+  static const mediumTextStyle = TextStyle(
+      fontFamily: "Medium"
+  );
+
+  static const semiBoldTextStyle = TextStyle(
+      fontFamily: "SemiBold"
+  );
+
+  static const boldTextStyle = TextStyle(
+      fontFamily: "Bold"
+  );
+}
