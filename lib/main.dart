6cb1ac0e53efbe366e5720275b29c76c3c597da2
memishood <emre.memis.49@gmail.com>
2021-12-20 23:01:21 +0300

    move countries screen to countries package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'features/countries_screen.dart';
+import 'features/countries/countries_screen.dart';
 
 /// @author emremms35@gmail.com
 void main() {
